dee8143fdad2ee0b2b55eec0cfd785face56f1cc
Leeshay <leeshaygobind@gmail.com>
2022-06-02 07:13:37 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * This is my first C program
- */
+ *  * main fucntion
+ *   * Return 0 for main
+ *    */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
