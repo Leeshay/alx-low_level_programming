1b6741cbecf4de01909be5e33f86c5065271e261
Leeshay <leeshaygobind@gmail.com>
2022-06-03 09:01:47 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - main function
+ * Return :nothing
+ */
+int main(){
+       char alpha = 'a';
+	while (alpha <='z'){
+		putchar(alpha);
+		putchar('\n');
+		alpha++;
+	}
+return (0);
+}
