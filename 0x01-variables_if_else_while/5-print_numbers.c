2b4d1a682631289492d9a159b7f500a184887c2a
Leeshay <leeshaygobind@gmail.com>
2022-06-03 10:21:38 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: print 0-9
+ * Return: 0
+ */
+int main(void)
+{
+	for (int i = 0; i <10; i++)
+	{
+		printf ("%i",i);
+	}
+	printf ('\n');
+	return (0);
+}
