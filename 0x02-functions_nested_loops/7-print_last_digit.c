3834dfd5532ee3544456094abb2de6848c6bc29e
Leeshay <leeshaygobind@gmail.com>
2022-06-07 11:18:50 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: integer
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{	int x;
+
+	if (n < 0)
+		x = -1 * (n % 10);
+	else
+		x = n % 10;
+	_putchar((x  % 10) + '0');
+	return (x % 10);
+}
