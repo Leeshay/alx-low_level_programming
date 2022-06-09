9f86db00afcc639569ca68a5b6958d923acead4c
Leeshay <leeshaygobind@gmail.com>
2022-06-09 07:21:11 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - print line of size n
+ * @n: integer
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n == 0)
+		_putchar('\n');
+	else
+		for (i = 0; i < n; i++)
+			_putchar(95);
+}
