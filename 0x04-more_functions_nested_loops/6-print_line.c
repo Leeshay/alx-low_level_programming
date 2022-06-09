4bb7623787c4394be5ab0619d8fd49044bf90d8d
Leeshay <leeshaygobind@gmail.com>
2022-06-09 08:22:51 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,10 +7,7 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
-		for (i = 0; i < n; i++)
-			_putchar(95);
+	for (i = 1; i <= n; i++)
+		_putchar(95);
 	_putchar('\n');
 }
