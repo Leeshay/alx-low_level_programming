c5607098255cd2dcf2b1f01cd2100f7bcf3b07cf
Leeshay <leeshaygobind@gmail.com>
2022-06-09 10:49:47 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,12 @@ void more_numbers(void)
 
 	for (i = 0; i <= 10; i++)
 	{
-		for ( j = '0'; j <= '14'; j++)
-		       _putchar(j);
-		/*
-		 * {
-		 *if (i >= 10)
-		 {
-		 _putchar((i / 10) + '0');
-		 }
-		 _putchar((i % 10) + '0');
-		 }
-		 */
-	_putchar('\n');
+		for (j = '0'; j <= '14'; j++)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+		}
+		_putchar('\n');
 	}
-}	
+}
