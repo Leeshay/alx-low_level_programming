ef5ed029ac39d7e704809723e0760f79e781a1d2
Leeshay <leeshaygobind@gmail.com>
2022-06-09 11:08:56 -0700

    task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	long int n = 612852475143;
+	int prime;
+
+	for (prime = 2; prime < n; prime++)
+	{
+		if (n % prime == 0)
+		{
+			n /= prime;
+			prime--;
+		}
+	}
+	printf("%ld\n", prime);
+	return (0);
+}
+
+
