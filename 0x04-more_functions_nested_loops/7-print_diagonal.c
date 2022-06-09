d68fb2ce5ebd470f96286a9b5343accb0b255528
Leeshay <leeshaygobind@gmail.com>
2022-06-09 08:29:30 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_diagonal - print line of size n
+ * @n: integer
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
